e7a123b2302895fb4d038332ead536b7135a4811
ihmaditaha <ihmaditaha1@gmail.com>
2025-09-08 19:43:08 +0100

    replacing the arrays with a structure

--- functions.c
+++ functions.c
@@ -1,14 +1,9 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "functions.h"
 
-char titres[30][50]; // the first one is the cells index and the second one is the string size
-char auteurs[30][50];
-float prixs[30];
-int quantites[30];
+livres livre[20];
 int stock = 0; //the number of books
 
+// menu to show the options in the menu
 void Menu(){
     system("cls"); // system allows us to write console commands and cls clears the screen
     printf("------- Menu: -------\n");
@@ -21,25 +16,24 @@ void Menu(){
     printf("Choisir : ");
 }
 
+// ajouter to add a book to the stock
 void Ajouter() {
     system("cls");
     printf("----- Ajout -----\n");
-    for (int i = stock; i <= stock; i++) {
         printf("Titre : ");
-        scanf("%[^\n]%*c", titres[i]); // %[^\n] allows us to input the whole line and %*c consumes the leftover new line
+        scanf("%[^\n]%*c", livre[stock].titre); // %[^\n] allows us to input the whole line and %*c consumes the leftover new line
         printf("Auteur : ");
-        scanf("%[^\n]%*c", auteurs[i]);
+        scanf("%[^\n]%*c", livre[stock].auteur);
         printf("Prix : ");
-        scanf("%f%*c", &prixs[i]);
+        scanf("%f%*c", &livre[stock].prix);
         printf("Quantite : ");
-        scanf("%d%*c", &quantites[i]);
+        scanf("%d%*c", &livre[stock].quantite);
         printf("\n");
-    }
     stock++;
     printf("Livre ajouté.\n");
     Attend();
 }
-
+// afficher to show the stock
 void Afficher() {
     system("cls");
     if (stock == 0) {
@@ -47,16 +41,16 @@ void Afficher() {
     } else {
         printf("----- Le Stock ----- \n");
         for (int i = 0; i < stock; i++) {
-            printf("Titre : %s\n", titres[i]);
-            printf("Auteur : %s\n", auteurs[i]);
-            printf("Prix : %.2f DH\n", prixs[i]);
-            printf("Quantité : %d unité\n", quantites[i]);
+            printf("Titre : %s\n", livre[i].titre);
+            printf("Auteur : %s\n", livre[i].auteur);
+            printf("Prix : %.2f DH\n", livre[i].prix);
+            printf("Quantité : %d unité\n", livre[i].quantite);
             printf("\n");
         }
     }
     Attend();
 }
-
+// searchalgo is the searching algorithm
 int SearchAlgo(){
     char titre[50];
     if (stock == 0) {
@@ -65,15 +59,14 @@ int SearchAlgo(){
     printf("Titre : ");
     scanf("%[^\n]%*c", titre);
         for (int i = 0; i < stock; i++) {
-            if (strcasecmp(titres[i], titre) == 0) { // strcasecmp is an insenstive strcmp
+            if (strcasecmp(livre[i].titre, titre) == 0) { // strcasecmp is an insenstive strcmp
                 return i;
-                break;
             }
         }
-        return -2;
+    return -2;
     }
 }
-
+// modifier to edit the quantity
 void Modifier() {
     system("cls");
     printf("----- modification -----\n");
@@ -84,17 +77,17 @@ void Modifier() {
         printf("Pas de livres dans le stock.\n");
     } else {
         printf("----- Le livre est trouvé -----\n");
-        printf("titre : %s\n", titres[s]);
-        printf("quantité : %d unité\n", quantites[s]);
+        printf("titre : %s\n", livre[s].titre);
+        printf("quantité : %d unité\n", livre[s].quantite);
         int nquant;
         printf("donnez la nouvelle quantité : ");
         scanf("%d%*c", &nquant);
-        quantites[s] = nquant;
+        livre[s].quantite = nquant;
         printf("quantité mise à jour avec succès.\n");
     }
     Attend();
 }
-
+// supprimer to delete a book from the stock
 void Supprimer() {
     system("cls");
     printf("---- Suppression -----\n");
@@ -104,18 +97,18 @@ void Supprimer() {
     } else if (s == -1){
         printf("Pas de livres dans le stock.\n");
     } else {
-        for (int j = s; j < stock - 1; j++) {
-            strcpy(titres[j], titres[j + 1]);
-            strcpy(auteurs[j], auteurs[j + 1]);
-            prixs[j] = prixs[j + 1];
-            quantites[j] = quantites[j + 1];
+        for (int i = s; i < stock - 1; i++) {
+            strcpy(livre[i].titre, livre[i + 1].titre);
+            strcpy(livre[i].auteur, livre[i + 1].auteur);
+            livre[i].prix = livre[i + 1].prix;
+            livre[i].quantite = livre[i + 1].quantite;
         }
         stock--;
         printf("Le livre est supprimé avec succès.\n");
     }
     Attend();
 }
-
+// affichertotale to add the quantities of all the books
 void AfficherTotale() {
     system("cls");
     int quantitetotal = 0;
@@ -123,14 +116,14 @@ void AfficherTotale() {
             printf("Pas de livres dans le stock.\n");
     } else {
         for (int i = 0; i < stock; i++) {
-            quantitetotal += quantites[i];
+            quantitetotal += livre[i].quantite;
         }
         printf("Le nombre total des livres en stock est : %d\n", quantitetotal);
     }
     Attend();
 }
 
-void Attend() { // this function is just there to make the program a bit professional hhh
+void Attend() { // this function is just there to make the program a bit professional
     printf("\n Cliquez sur ENTRER pour retourner à l'acceuil...");
     getchar();
 }
